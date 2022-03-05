644d76a6eebfc8b934a2a784d296dc3676e04374
neparkes-81 <nparkes8165@gmail.com>
2022-03-05 18:58:27 -0500

    Created methods for database of user data

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepositroy {
     constructor(filename){
@@ -12,10 +13,63 @@ class UsersRepositroy {
             fs.writeFileSync(this.filename, '[]')
         }
     }
-
     async getAll(){
-        
+        //Open file of this.filename, read data, parse data,  
+        return JSON.parse(await fs.promises.readFile(this.filename, {
+            encoding: 'utf8'
+        }));
+    }
+
+    async create(attrs) {
+        attrs.id = this.randomId();
+        const records = await this.getAll();
+        records.push(attrs);
+        await this.writeAll(records);
+    }
+
+    async writeAll(records){
+        // as we arent just trying to retirve info, but also add and update our data we need .writefile based in a promise
+        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex');
+    }
+
+    async getOne(id) {
+        const records = await this.getAll();
+        return records.find(record => record.id === id); 
+    }
+
+    async getOneBy( filter, searchTerm){
+        const records = await this.getAll();
+        return records.find(record => record[filter] === searchTerm); 
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRedcords = records.filter(record => record.id !== id);
+        await this.writeAll(filteredRedcords);
+    }
+
+    async update(id, attrs){
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+        // unlike the other methods this is not nesscarily demonstrating that some record was able to be found, so an error is needed to show an issue 
+        if (!record){
+            throw new Error(`Sorry record of id ${id} not found`)
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
     }
 }
 
-const repo = new UsersRepositroy('users.json');
+const test = async () => {
+    const repo = new UsersRepositroy('users.json');
+    await repo.update('3f310757', {password: 'password'});
+    // const user = await repo.getOneBy('password', 'word');
+    // console.log(user);
+}
+
+test();
